a786592a95864d5bbe99c0a24e4e3e304204e7b1
Jason <jason@thedatingcompany.net>
2020-10-26 12:58:02 -0400

    Minor img box tweaks; set up for adding future effects

---
+++ src/components/ImgEffect.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {CSSTransition} from 'react-transition-group';
+
+export default function ImgEffect(props) {
+
+	return (
+		<div className="img-effect">
+			&nbsp;
+		</div>
+	);
+}
